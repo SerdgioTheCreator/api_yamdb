7fc65fee3dbb4dd912c8f64454b7b0a1092febbc
Сергей Коновалов <SokiZNosoK68rus@yandex.ru>
2022-11-02 21:20:26 +0300

    edited Serializers

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -34,7 +34,9 @@ class ReviewViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         serializer.save(
             author=self.request.user,
-            title_id=self.kwargs.get('title_id')
+            title=get_object_or_404(
+                Title, pk=self.kwargs.get('title_id')
+            )
         )
 
 
