7eb12f658877731972b2e82370660ee0540a6181
Максим Акимов <introvert1989@yandex.ru>
2022-11-08 00:36:33 +0300

    Fixed quotes

--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -21,35 +21,36 @@ class User(AbstractUser):
         'Имя пользователя',
         max_length=AUTH_USERNAME_MAXLENGTH,
         unique=True,
-        help_text=(
-            'Обязательное поле. 150 символов или меньше'
-            'Латинские буквы, цифры и @/./+/-/_ .'
-        ),
         validators=(UsernameValidator(),),
-        error_messages={'unique': "Такой пользователь уже зарегистрирован."}
+        error_messages={'unique': "Такой пользователь уже зарегистрирован."},
+        verbose_name='Имя пользователя'
     )
     email = models.EmailField(
         'Адрес электронной почты',
         max_length=AUTH_EMAIL_MAXLENGTH,
         unique=True,
-        error_messages={'unique': "Такой адрес уже зарегистрирован."}
+        error_messages={'unique': "Такой адрес уже зарегистрирован."},
+        verbose_name='Адрес электронной почты'
     )
     bio = models.TextField(
         'О себе',
         blank=True,
-        null=True
+        null=True,
+        verbose_name='О себе'
     )
     role = models.CharField(
         'Ролевая группа',
         max_length=max(len(role[0]) for role in ROLE_CHOICES),
         choices=ROLE_CHOICES,
-        default=ROLE_USER
+        default=ROLE_USER,
+        verbose_name='Ролевая группа'
     )
     confirmation_code = models.CharField(
         'Код подтверждения',
         max_length=AUTH_CONF_CODE_MAXLENGTH,
         blank=True,
-        null=True
+        null=True,
+        verbose_name='Код подтверждения'
     )
 
     class Meta:
