737b12870007627167afc188de39547c0dbfc83a
Сергей Коновалов <SokiZNosoK68rus@yandex.ru>
2022-11-08 17:08:33 +0300

    merged with develop

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -30,7 +30,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'api'
 ]
 
 ROOT_URLCONF = 'api_yamdb.urls'
